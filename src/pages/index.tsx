df6687c7e35936fc888045c4118bb4d52a1a4c32
季生 <llabwing@gmail.com>
2020-06-14 14:53:19 +0800

    配置scss支持

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './index.less';
+import './index.less';
 
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className="normal">Page index</h1>
     </div>
   );
-}
+};
